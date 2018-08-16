94cd1f9a3ab9e6c66105b3d83ba076895ba1a073
Layz <Layz@gmail.com>
2018-08-16 19:49:32 +0700

    Sender can accept host and port

--- client.py
+++ client.py
@@ -2,15 +2,20 @@
 # -*- coding: UTF-8 -*-
 import socket
 
-def sender():
-    sendersock = socket.socket()
-    port = 8080
-    sendersock.connect(('localhost', port))
-    number = input('Please enter a number.\n')
-    sendersock.send(str(number).encode('utf-8'))
-    data = sendersock.recv(1024).decode('utf-8')
-    return(data)
-    sendersock.close()
+def sender(host = 'localhost', port = 8080):
+    try:
+        sendersock = socket.socket()
+        sendersock.connect((host, port))
+        number = input('Please enter a number.\n')
+        sendersock.send(str(number).encode('utf-8'))
+        data = sendersock.recv(1024).decode('utf-8')
+        return(data)
+    except ConnectionRefusedError:
+        print('Connection refused')
+    except ConnectionResetError:
+        print('Connection Reset')
+    finally:
+        sendersock.close()
 
 if __name__ == "__main__":
     while True:
