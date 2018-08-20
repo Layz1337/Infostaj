f6e4c408a36f113e5b620da763bc8afde8b40838
Layz <Layz@gmail.com>
2018-08-20 20:25:16 +0700

    New tests

--- tests.py
+++ tests.py
@@ -2,8 +2,8 @@ import unittest
 import server
 
 
-class ServerTests(unittest.TestCase):
-    def answertest(self):
+class TestServer(unittest.TestCase):
+    def test_answer(self):
         testvalues = {1:[1], 2:[2], 3:[3],
                       4:[2, 2], 8:[2, 2, 2], 16: [2, 2, 2, 2],
                       1024:[2, 2, 2, 2, 2, 2, 2, 2, 2, 2], 54689:[17, 3217],
@@ -11,8 +11,11 @@ class ServerTests(unittest.TestCase):
         for i in range(len(testvalues)):
             temp = testvalues.popitem()
             with self.subTest():
-                self.assertEqual(temp[0],temp[1])
-        
+                self.assertEqual(server.answer(temp[0]),temp[1])
+
+    def test_failure(self):
+        with self.assertRaises(TypeError):
+            server.answer('s')
 
 
 if __name__ == '__main__':
